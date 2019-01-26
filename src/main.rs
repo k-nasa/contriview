400cf7726201ec2457ba8941d81240e01b98b8f9
nasa <htilcs1115@gmail.com>
2019-01-26 23:28:30 +0900

    Add main function impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod contriview;
+
+use crate::contriview::ContriView;
+use reqwest::*;
+
 fn main() {
-    println!("Hello, world!");
+    // clapにしても良いかもね
+    let username = std::env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("Please input username");
+        std::process::exit(1);
+    });
+    let url = format!("https://github.com/users/{}/contributions", username);
+
+    let client = Client::new();
+    let mut resp = client.get(&url).send().unwrap();
+    let html = resp.text().unwrap();
+
+    println!("{:?}", ContriView::from_html(&html).unwrap())
 }

cf95877f369c6a542413681e2479da1b5dd66020
nasa <htilcs1115@gmail.com>
2019-01-26 22:19:08 +0900

    Add ContriView struct

---
+++ src/contriview.rs
@@ -0,0 +1,27 @@
+#[derive(Debug, Default, PartialEq, Eq)]
+pub struct ContriView {
+    today_contributions: u32,
+    week_contributions: u32,
+    month_contributions: u32,
+    sum_contributions: u32,
+}
+
+impl ContriView {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn contriview_default() {
+        assert_eq!(
+            ContriView::default(),
+            ContriView {
+                today_contributions: 0,
+                week_contributions: 0,
+                month_contributions: 0,
+                sum_contributions: 0
+            }
+        )
+    }
+}

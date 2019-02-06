0a2d0e6220b40a215f9510856ea27bc9b3cf39cf
nasa <htilcs1115@gmail.com>
2019-02-07 08:25:37 +0900

    feat: Print year contributions

--- src/contriview.rs
+++ src/contriview.rs
@@ -7,20 +7,23 @@ pub struct ContriView {
     today_contributions: u32,
     week_contributions: u32,
     month_contributions: u32,
+    year_contributions: u32,
     sum_contributions: u32,
 }
 
 impl ContriView {
     pub fn from_html(html: &str, date: Date<Local>) -> Result<Self, Error> {
         let sum_contributions = Self::sum_contributions_from_html(html);
-        let today_contributions = Self::today_contributions_from_html(html, date);
         let week_contributions = Self::week_contributions_from_html(html);
+        let year_contributions = Self::year_contributions_from_html(html, date);
         let month_contributions = Self::month_contributions_from_html(html, date);
+        let today_contributions = Self::today_contributions_from_html(html, date);
 
         Ok(ContriView {
             sum_contributions,
             week_contributions,
             month_contributions,
+            year_contributions,
             today_contributions,
         })
     }
@@ -78,6 +81,26 @@ impl ContriView {
         contributions.iter().rev().take(7).sum()
     }
 
+    fn year_contributions_from_html(html: &str, date: Date<Local>) -> u32 {
+        let doc = Html::parse_document(&html);
+
+        let now = date.format("%Y-").to_string();
+        let selector = format!("rect[data-date^=\"{}\"]", now);
+
+        let selector = Selector::parse(&selector).unwrap();
+        let input = doc.select(&selector);
+
+        let contributions: Vec<u32> = input
+            .into_iter()
+            .map(|i| -> u32 {
+                let contribution = i.value().attr("data-count").unwrap();
+                contribution.parse().unwrap_or_default()
+            })
+            .collect();
+
+        contributions.iter().sum()
+    }
+
     // FIXME use Result
     fn today_contributions_from_html(html: &str, date: Date<Local>) -> u32 {
         let doc = Html::parse_document(&html);
@@ -109,6 +132,7 @@ mod tests {
             ContriView {
                 today_contributions: 0,
                 week_contributions: 0,
+                year_contributions: 0,
                 month_contributions: 0,
                 sum_contributions: 0
             }
@@ -123,6 +147,7 @@ mod tests {
             ContriView {
                 sum_contributions: 3532,
                 month_contributions: 260,
+                year_contributions: 260,
                 week_contributions: 51,
                 today_contributions: 3
             }
@@ -149,6 +174,15 @@ mod tests {
         assert_eq!(
             3,
             ContriView::today_contributions_from_html(&sample_html(), date)
+        )
+    }
+    #[test]
+    fn test_year_contributions() {
+        let date = Local.ymd(2019, 1, 26);
+
+        assert_eq!(
+            260,
+            ContriView::year_contributions_from_html(&sample_html(), date)
         )
     }
 
